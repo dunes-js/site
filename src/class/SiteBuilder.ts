53d8e012b8feb2845a2241de18c112bc798e171e
Omar <omarfkuri@gmail.com>
2023-10-17 20:46:14 -0600

    Added parenthesis

--- src/class/SiteBuilder.ts
+++ src/class/SiteBuilder.ts
@@ -394,7 +394,7 @@ export class SiteBuilder {
     const paths: string[] = [];
     await trav(this.src(this.options.views.folder), {
       onFile: (parent, file) => {
-        paths.push("/" + join(parent, file.name)
+        paths.push(("/" + join(parent, file.name))
           .replace(/(\.\w+)+$/, "")
           .replace(/(?<=\/)index?$/, ""));
       }
